419ddee6a3c693400309b0f9b4678363039c1af4
PassQwerty <vdwork0@gmail.com>
2023-10-17 09:33:54 +0300

    add button save

--- components/ButtonSave.vue
+++ components/ButtonSave.vue
@@ -1,6 +1,44 @@
 <template>
-    <button class="buttonSave">Сохранить</button>
+    <button @click="tableSave" class="buttonSave">Сохранить</button>
 </template>
+
+<script setup>
+
+import { storeToRefs } from 'pinia'
+import { useTableStore } from '@/store/data'
+
+const store = useTableStore()
+const { tables } = storeToRefs(store)
+
+const tableSave = async () => {
+    if (tables.value.length > 0) {
+        
+        let resultTables = [];
+
+        for (let i = 0; i < tables.value.length; i++) {
+            const tablet = tables.value[i];
+            if(tablet.items.length > 0){
+                resultTables.push(tablet);
+            }
+        }
+
+        if(resultTables.length === 0) return;
+
+        const jsonData = JSON.stringify(resultTables);
+        const blob = new Blob([jsonData], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'data.json';
+
+        link.click();
+
+        URL.revokeObjectURL(url);
+    }
+}
+
+</script>
 
 <style scoped>
 .buttonSave {
